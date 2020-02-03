f27212ac1e67862972dc6ef2e190c307b413c7ed
farwayer <farwayer@gmail.com>
2020-02-03 14:53:33 +0300

    selector name

--- src/index.js
+++ src/index.js
@@ -6,9 +6,21 @@ export const StoreContext = createContext()
 export const StoreProvider = StoreContext.Provider
 
 export function select(Component, selector) {
-  return observer(props => {
+  const Selector = props => {
     const store = useContext(StoreContext)
     const finalProps = Object.assign({}, props, selector?.(store, props))
     return <Component {...finalProps}/>
-  })
+  }
+  Selector.displayName = selectorName(Component)
+
+  return observer(Selector)
+}
+
+function selectorName(component) {
+  const componentName =
+    component.displayName ||
+    component.name ||
+    component.constructor?.name ||
+    "Unknown"
+  return `select(${componentName})`
 }
